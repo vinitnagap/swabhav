b275903c211c532dfb913c0c2dd3668fd1fe09b9
vinitnagap <vinitnagap@gmail.com>
2020-04-08 20:18:39 +0530

    08 April 2020

--- OOP Assignment/Assignment7/src/com/techlabs/model/test/FooTest.java
+++ OOP Assignment/Assignment7/src/com/techlabs/model/test/FooTest.java
@@ -1,25 +1,33 @@
 package com.techlabs.model.test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import com.techlabs.model.*;
-import java.lang.reflect.*;
 
 public class FooTest {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+		Foo obj = new Foo();
 
-		doReflection(Foo.class);
-
+		getMethods(obj.getClass(), obj);
 	}
 
-	public static void doReflection(Class reflection) {
-		Method[] m = reflection.getDeclaredMethods();
-		int count = 0;
-		for (Method methods : m) {
-			if (methods.isAnnotationPresent(UnitTestCase.class)) {
-				count++;
+	public static void getMethods(Class className, Object obj)
+			throws IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException {
+
+		Method[] methods = className.getDeclaredMethods();
+		for (Method method : methods) {
+			if (method.isAnnotationPresent(UnitTestCase.class)) {
+				if ((boolean) method.invoke(obj)) {
+					System.out.println(method.getName() + " is passing case");
+				} else
+					System.out.println(method.getName() + " is failing case");
+			} else {
+				System.out.println(method.getName() + " is not a testcase");
 			}
-
 		}
-		System.out.println("No. of test cases is " + count);
-
 	}
+
 }
